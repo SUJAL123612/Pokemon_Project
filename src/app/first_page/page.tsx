51089d3a0afa5506a9c548db654e56e4930a1ac3
SuJaL3511 <95642419+SUJAL123612@users.noreply.github.com>
2025-09-05 20:18:14 +0530

    second_page complete

--- src/app/first_page/page.tsx
+++ src/app/first_page/page.tsx
@@ -1,24 +1,23 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
       <>
         <div className="w-full h-[592px] relative flex flex-col items-center">
-          {/* Logo + Text */}
           <div className="flex items-center space-x-4 -mt-[50px] ">
             <img
               src="logo.svg"
               alt="Logo"
               className="w-50 h-50"
             />
-            <span className="text-red-500 text-[50px] font-extrabold leading-tight">BATTLE ARENA</span>
+            <span className="text-red-500 text-[50px] font-extrabold leading-tight cursor-default">BATTLE ARENA</span>
           </div>
-  
-          {/* Background Image */}
           <img
             src="poke.jpg"
             alt="Pokemon"
             className="w-full h-[500px] object-contain mt-[-70px] "
           />
-          <p className="text-black text-[30px] mt-[-50px] cursor-pointer ml-3 font-bold hover:text-black/80">PLAY</p>
+          <Link className="text-black text-[30px] mt-[-50px] cursor-pointer ml-3 font-bold hover:text-black/70" href="/second_page">PLAY</Link>
         </div>
       </>
     );
---
+++ src/app/second_page/page.tsx
@@ -0,0 +1,39 @@
+export default function Name() {
+    return (
+        <>
+            <div className="w-full h-[592px] relative flex flex-col items-center mt-[-40px]">
+                <div className="flex items-center space-x-4">
+                    <img
+                        src="logo.svg"
+                        alt="Logo"
+                        className="w-50 h-50"
+                    />
+                    <span className="text-red-500 text-[50px] font-extrabold leading-tight cursor-default">
+                        BATTLE ARENA
+                    </span>
+                </div>
+            </div>
+            <div className="flex mt-[-300px] space-x-10 ml-[140px]">
+                <div className="w-[350px] h-[250px]">
+                    <img src="pikachu.png" alt="Pikachu" className="w-full h-full object-contain" />
+                </div>
+                <div className="flex flex-col space-y-4 relative z-10">
+                    <label htmlFor="player-1" className="text-lg font-semibold">
+                        Enter Player Name:
+                    </label>
+                    <input
+                        type="text"
+                        id="player-1"
+                        className="border border-gray-400 rounded px-3 py-2"
+                    />
+                    <button className="bg-red-600 text-white px-6 py-2 rounded hover:bg-red-400 hover:text-white/80 cursor-pointer">
+                        OK
+                    </button>
+                </div>
+                <div className="w-[450px] h-[450px] ml-[-45px] mt-[-130px]">
+                <img src="ash.png" alt="ASH" className="w-full h-full object-contain"/>
+            </div>
+            </div>
+        </>
+    );
+}
