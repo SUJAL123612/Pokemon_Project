524201946fd838a508a89d98e2b94a8c2ed3dea4
SuJaL3511 <95642419+SUJAL123612@users.noreply.github.com>
2025-09-07 16:34:41 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,14 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    rules: {
+      "@next/next/no-img-element": "off", 
+    },
   },
 ];
 
